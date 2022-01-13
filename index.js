6742731425510f918807ea35d8c280bd0261fac4
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-13 19:49:28 +0300

    done

--- index.js
+++ index.js
@@ -32,15 +32,74 @@ let cards = []; // You will use this array in step 6
 
 // Continue the code here 👇🏻
 
+///// ? 1. (100 points) Create a class Card that takes 2 properties `number`, and `color`
+///// ? 2. (100 points) Create the constructor inside the class `Card` that initializes color and number
+///// ? 5. (200 points) Add a method to class Card called `image()` that doesn't take any parameter, and it returns a string that represent the image path stored corresponding to card info
+class Card {
+	constructor(number, color) {
+		this.number = number;
+		this.color = color;
+	}
+	image() {
+		return (this.path = `./images/${this.color}_${this.number}.png`);
+	}
+}
+
+///// ? 3. (100 points) after creating the class, create 4 objects from the class Card for Green, Blue, Red, and Yellow
+// const Green = new Card(0, "Green");
+// const Blue = new Card(0, "Blue");
+// const Red = new Card(0, "Red");
+// const Yellow = new Card(0, "Yellow");
+
+///// ? 4. (50 points) console.log every single object.
+// console.log(Green);
+// console.log(Blue);
+// console.log(Red);
+// console.log(Yellow.image());
+
+///// ? 6. (100 points) Add 3 cards to the array `cards` defined below by using `.push` method on the cards array
+// cards.push(Green, Red, Yellow, Blue);
+
+///// ? 7. (1000 Points!)- great now you noticed that you created 4 objects manually, but that shouldn't be the case.
+let green;
+const greens = [];
+let blue;
+const blues = [];
+let red;
+const reds = [];
+let yellow;
+const yellows = [];
+for (let i = 0; i <= 9; i++) {
+	green = new Card(i, "Green");
+	greens.push(green);
+	green = new Card(i, "Green");
+	greens.push(green);
+	blue = new Card(i, "Blue");
+	blues.push(blue);
+	blue = new Card(i, "Blue");
+	blues.push(blue);
+	red = new Card(i, "Red");
+	reds.push(red);
+	red = new Card(i, "Red");
+	reds.push(red);
+	yellow = new Card(i, "Yellow");
+	yellows.push(yellow);
+	yellow = new Card(i, "Yellow");
+	yellows.push(yellow);
+}
+
+cards.push(...greens, ...blues, ...reds, ...yellows);
+cards = cards.sort(() => Math.random() - 0.5);
+
 /**
  *
  * Don't touch this code
  * This function is going to render the cards array to the HTML page,
  */
 function render(array) {
-  document.getElementById("cards-container").innerHTML = array
-    .map(cardComponent)
-    .join("");
+	document.getElementById("cards-container").innerHTML = array
+		.map(cardComponent)
+		.join("");
 }
 
 render(cards);
